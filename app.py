98e72af2b4ab4be3fc7158893fc487df30b0590f
Zaki Abdulle <zakiabdulle1@gmail.com>
2021-10-03 21:57:13 +0100

    saving progress

--- app.py
+++ app.py
@@ -1,3 +1,25 @@
 import numpy as np
 
-a = np.array([1, 2, 3])
+
+def empty_board():
+    """Returns empty 8x8 board"""
+    return np.empty(shape=(8, 8), dtype=object)
+
+
+def new_board():
+    """create an 8x8 board with chess positions"""
+    board = empty_board()
+    letters = ["A", "B", "C", "D", "E", "F", "G", "H"]
+    for i in range(8):
+        board[i] = [{(str(i) + char): ""} for char in letters]
+    return board
+
+
+def main():
+    test = new_board()
+    print(test)
+
+
+if __name__ == "__main__":
+    np.set_printoptions(linewidth=1000)  # increase terminal width
+    main()
